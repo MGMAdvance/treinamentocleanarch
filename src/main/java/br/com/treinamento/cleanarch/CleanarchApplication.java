6de7376187141268ca331d21287f6246a0cd91cb
Matheus Gonçalves <matheusgt4@hotmail.com>
2020-04-01 22:37:00 -0300

    Començando a Aventura

---
+++ src/main/java/br/com/treinamento/cleanarch/CleanarchApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.cleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinamento/cleanarch/CleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
